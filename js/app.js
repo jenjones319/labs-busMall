7cd8785eacc09faadf27d7fb1b98256dbadb35a5
jenjones319 <jenjones605@gmail.com>
2020-06-30 23:01:55 -0500

    added chart js

--- js/app.js
+++ js/app.js
@@ -139,10 +139,57 @@ function imageWasClicked (event) {
       imageElements[i].removeEventListener('click', imageWasClicked)
     }
 
-  // runMyChartNow()
   }
 }
 for (var i = 0; i < imageElements.length; i++) {
   console.log('this is the event listener for the click image event.')
   imageElements[i].addEventListener('click', imageWasClicked)
 }
+function runMyChartNow() {
+}//closing the if total rounds is >= rounds
+
+
+
+
+var ctx = document.getElementById('resultsChart').getContext('2d');
+
+
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: getProductsArray('name'),
+    datasets: [{
+      label: '# of Votes',
+      //what does this data do? 
+      //actually the valies in the chart
+      data: getProductsArray('timesClicked'),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 3
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
